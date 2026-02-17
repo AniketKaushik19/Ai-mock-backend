3c3cec28f6af04ebb31a39c5b6c102e4df09fd04
AniketKaushik19 <aniketsskaushik@gmail.com>
2026-02-17 10:18:00 +0530

    change ai model resume

--- src/aiagents/QuestionAgent.js
+++ src/aiagents/QuestionAgent.js
@@ -170,7 +170,7 @@ export async function analyzeResumeWithAI({ resumeText, enableAutoRewrite }) {
     ];
 
     const response = await groq.chat.completions.create({
-      model: "openai/gpt-oss-120b",
+      model: "llama-3.3-70b-versatile",
       temperature: 0.150,
       top_p: 1,
       messages,
